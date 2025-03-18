d23c4e9d7b2979a20726ee60be70b0f4988c27c3
Shilpa-Susan <shilpasusanabraham@gmail.com>
2025-03-18 01:57:25 +0000

    Username and password added

--- src/test/java/TestDemo/OrangeHRM.java
+++ src/test/java/TestDemo/OrangeHRM.java
@@ -1,5 +1,6 @@
 package TestDemo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -35,7 +36,13 @@ public class OrangeHRM {
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
 		String Title=driver.getTitle();
-		Assert.assertTrue(Title.contains("Orange"),"Title doesnot match");
+		Assert.assertTrue(Title.contains("OrangeHRM"));
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		Thread.sleep(3000);
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+		Thread.sleep(3000);
 		driver.quit();
 	}
 
